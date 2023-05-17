60cc63366039c87f8066e09ef098c4a3bfcf111b
sobhanshukueian <sobhan.shu33@gmail.com>
2023-05-17 15:57:44 +0330

    sasa

--- BYOL.py
+++ BYOL.py
@@ -101,13 +101,13 @@ class Trainer:
 
 
         # get model 
-        self.model = BYOLNetwork().cuda()
+        self.model, self.conf, self.ckpt = get_model("byol", self.conf, self.resume, self.resume_dir, self.weights, self.verbose)
+        self.model = self.model.to(device)
         if self.verbose > 2:
             self.conf = count_parameters(self.model, self.conf)
 
-        # Get optimizer
+        self.optimizer, self.conf = get_optimizer(self.model.parameters(), self.conf, self.resume, self.ckpt, optimizer=OPTIMIZER, lr0=model_config["LEARNING_RATE"], momentum=model_config["MOMENTUM"], weight_decay=model_config["WEIGHT_DECAY"], verbose=self.verbose)
         self.optimizer = torch.optim.SGD(get_params_groups(self.model), lr=0.06, weight_decay=5e-4, momentum=0.9)
-
 
         if self.resume:
             self.start_epoch = self.ckpt["epoch"] + 1
