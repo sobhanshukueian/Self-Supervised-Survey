e75091f03236d96a81118013783ee7ef0387222f
sobhanshukueian <sobhan.shu33@gmail.com>
2023-07-13 21:36:25 +0330

    changed simsiam loss function

--- models/simsiam.py
+++ models/simsiam.py
@@ -14,7 +14,30 @@ from models.layers import MLP
 import copy
 from models.layers import ModelBase
 
+class SimSiamLoss(nn.Module):
+    def __init__(self, version='simplified'):
+        super().__init__()
+        self.ver = version
 
+    def asymmetric_loss(self, p, z):
+        if self.ver == 'original':
+            z = z.detach()  # stop gradient
+
+            p = nn.functional.normalize(p, dim=1)
+            z = nn.functional.normalize(z, dim=1)
+
+            return -(p * z).sum(dim=1).mean()
+
+        elif self.ver == 'simplified':
+            z = z.detach()  # stop gradient
+            return - nn.functional.cosine_similarity(p, z, dim=-1).mean()
+
+    def forward(self, z1, z2, p1, p2):
+
+        loss1 = self.asymmetric_loss(p1, z2)
+        loss2 = self.asymmetric_loss(p2, z1)
+
+        return 0.5 * loss1 + 0.5 * loss2
 
 
 class SimSiam_MODEL(nn.Module):
@@ -29,7 +52,7 @@ class SimSiam_MODEL(nn.Module):
         super(SimSiam_MODEL, self).__init__()
 
         # create the encoder
-        self.backbone = ModelBase(feature_dim=dim, arch=arch)
+        self.encoder = ModelBase(feature_dim=dim, arch=arch)
 
         self.projector = MLP(dim, dim)
 
@@ -38,15 +61,13 @@ class SimSiam_MODEL(nn.Module):
             self.projector
         )'''
 
-        self.encoder = self.backbone
-
         # build a 2-layer predictor
         self.predictor = nn.Sequential(nn.Linear(dim, model_config["HIDDEN_SIZE"], bias=False),
                                         nn.BatchNorm1d(model_config["HIDDEN_SIZE"]),
                                         nn.ReLU(inplace=True), # hidden layer
                                         nn.Linear(model_config["HIDDEN_SIZE"], dim)) # output layer
 
-        self.criterion = nn.CosineSimilarity(dim=1).to(model_config["device"])
+        self.criterion = SimSiamLoss(args.loss_version)
 
     def forward(self, x1, x2, train=False):
         """
@@ -68,6 +89,6 @@ class SimSiam_MODEL(nn.Module):
         p1 = self.predictor(h1) # NxC
         p2 = self.predictor(h2) # NxC
 
-        loss = -(self.criterion(p1, z2.detach()).mean() + self.criterion(p2, z1.detach()).mean()) * 0.5
+        loss = criterion(z1, z2, p1, p2)
 
         return (z1, p1), loss, [loss, loss, loss]
